fd7f453d0ee576e66268b3daba4dc0dedd1e9f86
Arrragorn <chevreuil768@gmail.com>
2020-09-12 15:22:20 -0400

    test fonctionnel

--- Lab1_2/Lab1_2/Lab1.cpp
+++ Lab1_2/Lab1_2/Lab1.cpp
@@ -1,34 +1,38 @@
+
+
+#include <iostream>
 #include <dshow.h>
 
-void main(void)
+int main()
 {
-    IGraphBuilder *pGraph = NULL;
-    IMediaControl *pControl = NULL;
-    IMediaEvent   *pEvent = NULL;
+    std::cout << "Hello World!\n";
+    IGraphBuilder* pGraph = NULL;
+    IMediaControl* pControl = NULL;
+    IMediaEvent* pEvent = NULL;
 
     // Initialize the COM library.
     HRESULT hr = CoInitialize(NULL);
     if (FAILED(hr))
     {
         printf("ERROR - Could not initialize COM library");
-        return;
+        return 1;
     }
 
     // Create the filter graph manager and query for interfaces.
-    hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER, 
-                        IID_IGraphBuilder, (void **)&pGraph);
+    hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER,
+        IID_IGraphBuilder, (void**)&pGraph);
     if (FAILED(hr))
     {
         printf("ERROR - Could not create the Filter Graph Manager.");
-        return;
+        return 1;
     }
 
-    hr = pGraph->QueryInterface(IID_IMediaControl, (void **)&pControl);
-    hr = pGraph->QueryInterface(IID_IMediaEvent, (void **)&pEvent);
+    hr = pGraph->QueryInterface(IID_IMediaControl, (void**)&pControl);
+    hr = pGraph->QueryInterface(IID_IMediaEvent, (void**)&pEvent);
 
     // Build the graph. IMPORTANT: Change this string to a file on your system.
-    hr = pGraph->RenderFile(L"C:\\Example.avi", NULL);
-    
+    hr = pGraph->RenderFile(L"Example.avi", NULL);
+
     if (SUCCEEDED(hr))
     {
         // Run the graph.
@@ -43,9 +47,11 @@ void main(void)
             // can block indefinitely.
         }
     }
-    
+
     pControl->Release();
     pEvent->Release();
     pGraph->Release();
     CoUninitialize();
+
+    return 0;
 }
